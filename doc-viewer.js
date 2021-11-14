44ff507518115704ed169b16827c568bec864aad
Nacho <67743899+isanchop@users.noreply.github.com>
2021-11-14 23:33:29 +0100

    Update 0.3.0

--- doc-viewer.js
+++ doc-viewer.js
@@ -1,18 +1,19 @@
 window.addEventListener('load', function(){
 	window.addEventListener('click', function(evt){
 		if(evt.detail === 3){
-			var src=[];
+
+			var head = document.getElementsByTagName("head")[0].innerHTML;
+			var pdf = document.getElementById('page-container-wrapper').parentNode.innerHTML;
+
+			newWindow = window.open("", "Document", "height=865,width=625,status=yes,toolbar=no,menubar=no");  
+			newWindow.document.getElementsByTagName("head")[0].innerHTML = head + "<style> div {filter: none !important;} </style>";
+			newWindow.document.getElementsByTagName("body")[0].innerHTML = pdf;
+			newWindow.document.getElementsByTagName("body")[0].childNodes[0].style = "transform: scale(1); width: 100%; height: 100%;";
 			var pages = document.getElementById('page-container').childNodes;
-			for(i=0; i<pages.length; i++){
-				src[i]=pages[i].childNodes[0].childNodes[0].getAttribute('src');
-			}
-			newWindow = window.open("", "Document", "height=850,width=600,status=yes,toolbar=no,menubar=no");  
-		
-			for(i=0; i<pages.length; i++) {
-				newWindow.document.write("<img src=\"");  
-				newWindow.document.write(src[i]);  
-				newWindow.document.write("\" style=\"width: 100%\">");
-			}
+    	for(i=0; i<pages.length; i++){
+        	pages[i].classList.add("nofilter");
+    	}
+
 		}
 	});
 });
