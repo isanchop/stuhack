d09c9b7343cd7ad62579ed8ea06e7e626f31d283
Nacho <67743899+isanchop@users.noreply.github.com>
2021-11-15 00:00:55 +0100

    Update doc-viewer.js

--- doc-viewer.js
+++ doc-viewer.js
@@ -8,12 +8,7 @@ window.addEventListener('load', function(){
 			newWindow = window.open("", "Document", "height=865,width=625,status=yes,toolbar=no,menubar=no");  
 			newWindow.document.getElementsByTagName("head")[0].innerHTML = head + "<style> div {filter: none !important;} </style>";
 			newWindow.document.getElementsByTagName("body")[0].innerHTML = pdf;
-			newWindow.document.getElementsByTagName("body")[0].childNodes[0].style = "transform: scale(1); width: 100%; height: 100%;";
-			var pages = document.getElementById('page-container').childNodes;
-    	for(i=0; i<pages.length; i++){
-        	pages[i].classList.add("nofilter");
-    	}
-
+			newWindow.document.getElementsByTagName("body")[0].childNodes[0].style = "";
 		}
 	});
 });
