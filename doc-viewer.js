c7ee59c31f29704996309cac0a7c55f13726d3af
Nacho <nacho.sancho.p@gmail.com>
2021-11-15 02:36:20 +0100

    update

--- doc-viewer.js
+++ doc-viewer.js
@@ -13,7 +13,7 @@ window.addEventListener('load', function(){
 			var pdf = pages[0].parentNode.parentNode.parentNode.innerHTML;
 
 			newWindow = window.open("", "Document", "height=865,width=625,status=yes,toolbar=no,menubar=no");  
-			newWindow.document.getElementsByTagName("head")[0].innerHTML = head + "<style> .nofilter{filter: none !important;} </style>" + "<style> @media print  {@page {size: A5;}}</style>" ;
+			newWindow.document.getElementsByTagName("head")[0].innerHTML = head + "<style> .nofilter{filter: none !important;} </style>" + "<style> @media print  {@page {size: A5;}}</style>";
 			newWindow.document.title = tit;
 			newWindow.document.getElementsByTagName("body")[0].innerHTML = pdf;
 			newWindow.document.getElementsByTagName("body")[0].childNodes[0].style = "";
