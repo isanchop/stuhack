849d6015fe1f9b340bbebe9db65f32b2e9f93f51
Nacho <67743899+isanchop@users.noreply.github.com>
2021-11-15 02:11:14 +0100

    Update 0.3.4
    Better print

--- doc-viewer.js
+++ doc-viewer.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function(){
 		if(evt.detail === 3){
 
 			var head = document.getElementsByTagName("head")[0].innerHTML;
-			
+			var tit = document.getElementsByTagName("h1")[0].innerHTML;
 			var pages = document.getElementById('page-container').childNodes;
 
 			for(i=0; i<pages.length; i++){
@@ -14,6 +14,7 @@ window.addEventListener('load', function(){
 
 			newWindow = window.open("", "Document", "height=865,width=625,status=yes,toolbar=no,menubar=no");  
 			newWindow.document.getElementsByTagName("head")[0].innerHTML = head + "<style> .nofilter{filter: none !important;} </style>" + "<style> @media print  {@page {size: A5;}}</style>" ;
+			newWindow.document.title = tit;
 			newWindow.document.getElementsByTagName("body")[0].innerHTML = pdf;
 			newWindow.document.getElementsByTagName("body")[0].childNodes[0].style = "";
 		}
