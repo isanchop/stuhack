6079725012ec2d9bdc38a3041608a3498f56fcff
Nacho <nacho.sancho.p@gmail.com>
2022-01-11 21:15:22 +0100

    Update 0.3.6 minor update fixing bugs

--- remove-banner.js
+++ remove-banner.js
@@ -2,6 +2,8 @@ window.addEventListener('load', function(){
 	var element = document.getElementById('viewer-wrapper')
 	if(element != null){
 		var elements = element.childNodes;
-		elements[3].parentNode.removeChild(elements[3]);
+		if (elements.length>3){
+			elements[3].parentNode.removeChild(elements[3]);
+		}
 	}		
 });
