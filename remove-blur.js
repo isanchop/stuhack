9af8edf21ba59a10f1ed436f4b1b055740fbc193
Nacho <nacho.sancho.p@gmail.com>
2022-01-12 18:30:13 +0100

    Update 0.3.7 remove new banner on top of the blured pages

--- remove-blur.js
+++ remove-blur.js
@@ -1,6 +1,12 @@
 window.addEventListener('load', function(){
     var pages = document.getElementsByClassName('page-content');
     for(i=0; i<pages.length; i++){
+        pagecontent=pages[i].parentNode.childNodes;
+        for(j=0; j<pagecontent.length; j++){
+            if(pagecontent[j].className != "page-content"){
+                pagecontent[j].parentNode.removeChild(pagecontent[j]);
+            }
+        }
         pages[i].classList.add("nofilter");
     }
 });
