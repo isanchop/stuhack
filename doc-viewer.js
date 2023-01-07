2d741818abf98386fe17f5bda7e4930f1d6521b8
Nacho <nacho.sancho.p@gmail.com>
2023-01-07 23:00:30 +0100

    Update v0.4.9 - Automatically download as landscape

--- doc-viewer.js
+++ doc-viewer.js
@@ -30,6 +30,15 @@ window.addEventListener('load', function(){
 			var tit = document.getElementsByTagName("h1")[0].innerHTML;
 			var pages = document.getElementById('page-container').childNodes;
 
+			width = pages[0].offsetWidth;
+			height = pages[0].offsetHeight;
+
+			if (width > height){
+				print_opt = "{@page {size: A5 landscape;} body {zoom: 90%;}";
+			}else{
+				print_opt = "{@page {size: A5 portrait;}";
+			}
+
 			for(i=0; i<pages.length; i++){
 				pages[i].childNodes[0].style = "display: block;";
 			}
@@ -37,7 +46,7 @@ window.addEventListener('load', function(){
 			var pdf = pages[0].parentNode.parentNode.parentNode.innerHTML;
 
 			newWindow = window.open("", "Document", "height=865,width=625,status=yes,toolbar=no,menubar=no");  
-			newWindow.document.getElementsByTagName("head")[0].innerHTML = head + "<style> .nofilter{filter: none !important;} </style>" + "<style> @media print  {@page {size: A5;}}</style>";
+			newWindow.document.getElementsByTagName("head")[0].innerHTML = head + "<style> .nofilter{filter: none !important;} </style>" + "<style> @media print " + print_opt + "</style>";
 			newWindow.document.title = tit;
 			newWindow.document.getElementsByTagName("body")[0].innerHTML = pdf;
 			newWindow.document.getElementsByTagName("body")[0].childNodes[0].style = "";
