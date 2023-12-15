567c7e8fc5d7b160c2e6fe290cec3dc21397d57b
isanchop <nacho.sancho.p@gmail.com>
2023-12-15 13:54:36 -0300

    Update v0.5.6 - fix blured images

--- remove-blur.js
+++ remove-blur.js
@@ -1,3 +1,12 @@
+const focusImages = () => {
+    var bluredContainers = Array.from(document.getElementsByClassName('blurred-container'));
+    bluredContainers.forEach( (bluredContainer) => {
+        bluredContainer.firstChild.src = bluredContainer.firstChild.src.replace('/blurred/', '/');
+        bluredContainer.firstChild.classList.add('bi', 'x0', 'y0', 'w1', 'h1');
+        bluredContainer.classList.remove('blurred-container');
+    });
+}
+
 window.addEventListener('load', function(){
     var pages = document.getElementsByClassName('page-content');
     for(i=0; i<pages.length; i++){
@@ -9,17 +18,6 @@ window.addEventListener('load', function(){
         }
         pages[i].classList.add("nofilter");
     }
-
-    document.getElementById('viewer-wrapper').addEventListener('scroll', () => {
-        var bluredContainers = Array.from(document.getElementsByClassName('blurred-container'));
-        bluredContainers.forEach( (bluredContainer) => {
-            bluredContainer.firstChild.src = bluredContainer.firstChild.src.replace('/blurred/', '/');
-            bluredContainer.firstChild.classList.add('bi', 'x0', 'y0', 'w1', 'h1');
-            console.log(bluredContainer.firstChild.src);
-            bluredContainer.classList.remove('blurred-container');
-
-        });
-     });
-
-    
+    document.getElementById('viewer-wrapper').addEventListener('scroll', () => {focusImages()});
+    document.getElementById('document-wrapper').addEventListener('scroll', (e) => { focusImages()});
 });
